15bdd2a557bb90d8fe0b69d7b3c9ff593438f308
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2023-09-01 19:17:05 -0300

    mensagem casal page gifts

--- src/app/(pages)/gifts/page.tsx
+++ src/app/(pages)/gifts/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import data from "@/json/data.json";
+import { ChevronUpIcon } from "@heroicons/react/24/outline";
 
 const Gifts = () => {
-  return (
-    <div>Gifts</div>
-  )
-}
+  const [isOpen, setIsOpen] = React.useState(false);
 
-export default Gifts
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <div
+        onClick={handleClick}
+        className="cursor-pointer w-full flex justify-between text-2xl border border-quaternary p-2 rounded-lg"
+      >
+        Mensagem do casal{" "}
+        <button
+          type="button"
+          className="bg-quaternary rounded-full flex justify-center items-center p-1 h-8 w-8"
+        >
+          <ChevronUpIcon
+            className={`${
+              isOpen
+                ? "duration-300 ease-in-out rotate-180"
+                : "duration-300 ease-in-out rotate-0"
+            } m-0`}
+          />
+        </button>
+      </div>
+      <h1
+        className={`${
+          isOpen
+            ? "opacity-100 max-h-full duration-500 ease-in-out"
+            : "opacity-0 max-h-0 duration-500 ease-in-out"
+        } text-justify`}
+      >
+        {data.texto_geral.texto}
+      </h1>
+    </div>
+  );
+};
+
+export default Gifts;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,20 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
+import data from "@/json/data.json";
 
 const Home = () => {
   const NoSSR = dynamic(() => import("@/components/Countdown"), { ssr: false });
-  const targetDate = new Date("2024-08-24 00:00:00");
+  const targetDate = new Date(`${data.data_casamento} 00:00:00`);
 
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-9xl ">
-        Denis <span className="text-7xl">&</span>
+        {data.nome_noivo} <span className="text-7xl">&</span>
       </h1>
-      <h1 className="text-9xl ml-24">Letícia.</h1>
+      <h1 className="text-9xl ml-24">{data.nome_noiva}.</h1>
       <div className="border-quaternary border-y-2 mt-4 w-4/5 sm:w-2/4 lg:w-4/12 flex justify-center">
-        <h2 className="text-6xl my-4">24.08.2024</h2>
+        <h2 className="text-6xl my-4">{data.data_casamento}</h2>
       </div>
       <div className="mt-8">
         <p className="text-2xl">Contagem regressiva para o grande dia.</p>
