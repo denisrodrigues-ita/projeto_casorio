a185d9bd53fd3d75ce94f83dccb8fc482c1a4ccc
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2023-10-06 20:20:53 -0300

    feat toast component

--- src/app/(pages)/confirm-presence/page.tsx
+++ src/app/(pages)/confirm-presence/page.tsx
@@ -1,42 +1,86 @@
 "use client";
 import React from "react";
 import { api } from "@/services";
+import { Toast } from "@/components";
 
 const ConfirmPresence = () => {
+  const [isToastOpen, setIsToastOpen] = React.useState(false);
+  const [message, setMessage] = React.useState("");
+  const [color, setColor] = React.useState("");
   const [code, setCode] = React.useState("");
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsToastOpen(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [isToastOpen]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (code === "") return;
-    await api.put(code);
+    const isValidCode = handleCodeVerification(code);
+    if (!isValidCode) return;
+    const { response, result } = await api.put(code);
+    handleResultValidation(response, result);
     setCode("");
   };
 
+  const handleCodeVerification = (code: string) => {
+    if (code.length != 6) {
+      setIsToastOpen(true);
+      setMessage("O código deve conter 6 dígitos");
+      setColor("yellow");
+      return false;
+    }
+    return true;
+  };
+
+  const handleResultValidation = (response: Response, result?: any) => {
+    if (response.ok) {
+      setIsToastOpen(true);
+      setMessage(`${result.name}, sua presença foi confirmada!`);
+      setColor("green");
+      return;
+    }
+    setIsToastOpen(true);
+    setMessage("Código inválido");
+    setColor("red");
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1>Confirmar Presença</h1>
-      <p className="text-center my-8">
-        Pedimos a confirmação da presença para que possamos nos preparar da
-        melhor forma para te receber.
-      </p>
-      <form action="submit" onClick={handleSubmit}>
-        <fieldset className="border border-primary rounded-lg p-4 flex flex-col gap-4">
-          <legend className="">Confirme sua presença</legend>
-          <label>
-            Código de Confirmação:
-            <input
-              type="text"
-              placeholder="Código de Confirmação"
-              value={code}
-              onChange={(e) => setCode(e.target.value)}
-              className="p-2 rounded-lg block text-quinary font-semibold"
-            />
-          </label>
-          <button type="submit" className="btn1">
-            Confirmar
-          </button>
-        </fieldset>
-      </form>
+    <div className="relative">
+      {isToastOpen && (
+        <Toast
+          setIsToastOpen={setIsToastOpen}
+          message={message}
+          color={color}
+        />
+      )}
+      <div className="flex flex-col justify-center items-center">
+        <h1>Confirmar Presença</h1>
+        <p className="text-center my-8">
+          Pedimos a confirmação da presença para que possamos nos preparar da
+          melhor forma para te receber.
+        </p>
+        <form action="submit" onSubmit={handleSubmit}>
+          <fieldset className="border border-primary rounded-lg p-4 flex flex-col gap-4">
+            <legend className="">Confirme sua presença</legend>
+            <label>
+              Código de Confirmação:
+              <input
+                type="text"
+                placeholder="Código de Confirmação"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+                className="p-2 rounded-lg block text-quinary font-semibold"
+              />
+            </label>
+            <button type="submit" className="btn1">
+              Confirmar
+            </button>
+          </fieldset>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Countdown } from "./Countdown";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Modal } from "./Modal";
+export { default as Toast } from "./Toast";
